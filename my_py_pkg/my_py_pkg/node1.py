ecab4bc88f5df2d117384da8c441caac41231f3b
Kushagra340 <kushagra2503@gmail.com>
2026-05-19 00:40:09 +0530

    Updated code for OOP in node1

--- my_py_pkg/my_py_pkg/node1.py
+++ my_py_pkg/my_py_pkg/node1.py
@@ -2,11 +2,20 @@
 import rclpy
 from rclpy.node import Node
 
+class Node1(Node):
+    def __init__(self):
+        super().__init__("node1")
+        self.counter_ = 0
+        self.get_logger().info("Hello World!!")
+        self.create_timer(1.0, self.timer_callback)
+
+    def timer_callback(self):
+        self.get_logger().info("Hello" + str(self.counter_))
+        self.counter_ += 1
 
 def main(args=None):
     rclpy.init(args=args)
-    node=Node("node1")
-    node.get_logger().info("Hello World!!")
+    node=Node1()
     rclpy.spin(node)
     rclpy.shutdown()
 
