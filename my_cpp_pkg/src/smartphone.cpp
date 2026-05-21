2ead12bceea9380146b3f4ad350839b4b2721a08
Kushagra340 <kushagra2503@gmail.com>
2026-05-21 17:40:33 +0530

    Added examples for ROS2 topics in cpp

---
+++ my_cpp_pkg/src/smartphone.cpp
@@ -0,0 +1,28 @@
+#include "rclcpp/rclcpp.hpp"
+#include "example_interfaces/msg/string.hpp"
+
+class SmartphoneNode : public rclcpp::Node 
+{
+public:
+    SmartphoneNode() : Node("smartphone") 
+    {
+        subscriber_ = this->create_subscription<example_interfaces::msg::String>("robot_news", 10, std::bind(&SmartphoneNode::callbackRobotNews, this, std::placeholders::_1));
+        RCLCPP_INFO(this->get_logger(), "Smartphone Node has been started and is listening to 'robot_news' topic.");
+    }
+
+private:
+    void callbackRobotNews(const example_interfaces::msg::String::SharedPtr msg)
+    {
+        RCLCPP_INFO(this->get_logger(), "Received news: '%s'", msg->data.c_str());
+    }
+    rclcpp::Subscription<example_interfaces::msg::String>::SharedPtr subscriber_;
+};
+
+int main(int argc, char **argv)
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<SmartphoneNode>(); 
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}
